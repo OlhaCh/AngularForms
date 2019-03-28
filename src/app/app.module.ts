c47aead5a5fe2b62182f6de0b42c9929a090e626
Olha Chebotarova <chebotarovaolha@gmail.com>
2019-03-28 13:02:58 +0200

    add all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicformComponent } from './basicform/basicform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicformComponent
   ],
   imports: [
     BrowserModule,
