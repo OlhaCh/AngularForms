8bf702f216b5fd72d976d720e80b11ebdc435ac2
Olha Chebotarova <chebotarovaolha@gmail.com>
2019-03-28 14:28:52 +0200

    Basic form

--- src/app/basicform/basicform.component.ts
+++ src/app/basicform/basicform.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basicform.component.css']
 })
 export class BasicformComponent implements OnInit {
-
+languages = ['English', 'Spanish', 'Other'];
   constructor() { }
 
   ngOnInit() {
